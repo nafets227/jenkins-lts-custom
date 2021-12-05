d373ba28f8c6c8c3969d0bfbf0f77e8ab071cf0d
Stefan Schallenberg <nafets227@users.noreply.github.com>
2021-12-05 20:43:46 +0100

    add test_bootstrap

--- test/test1.sh
+++ test/test1.sh
@@ -15,7 +15,16 @@ function test_emptyhelm {
     return 0
 }
 
-function test_2 {
+function test_bootstrap {
+    # autoupdate with just bootstrap helm chart should work
+    export GITHUB_REF="refs/heads/$(git branch --show-current)"
+
+    test_exec_simple "mkdir -p charts/jenkins-lts-custom"
+    test_exec_simple "cp -a $BASEDIR/jenkins-lts-custom.bootstrap/* charts/jenkins-lts-custom"
+    test_exec_simple ".github/workflows/autoupdate.sh" &&
+        test_lastoutput_contains "::set-output name=newVersion::0.0.2-chartsubmodule-localtest.$(date "+%Y%m%d")"
+    export GITHUB_REF=
+
     return 0
 }
 
