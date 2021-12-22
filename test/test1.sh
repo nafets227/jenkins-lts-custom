00476d8a1476a8712fadc005c4cac7267c69f897
Stefan Schallenberg <nafets227@users.noreply.github.com>
2021-12-22 19:44:16 +0100

    refactor tests
    no longer use our git repo but create a new one for testing.
    This prohibits any unwanted changes.

--- test/test1.sh
+++ test/test1.sh
@@ -4,6 +4,7 @@
 #
 
 function test_emptyhelm {
+    git checkout -q -b "$FUNCNAME" &&
     test_exec_simple "rm -rf charts/jenkins-lts-custom" "0" &&
         test_expect_file_missing "charts/jenkins-lts-custom" &&
         true || return 1
@@ -11,19 +12,48 @@ function test_emptyhelm {
     # autoupdate without any helm chart should fail
     test_exec_simple ".github/workflows/autoupdate.sh" "1" &&
         test_expect_file_missing "charts/jenkins-lts-custom" &&
+        test_expect_value "$(git status --porcelain)" \
+            " D charts/jenkins-lts-custom/Chart.yaml" &&
 
     return 0
 }
 
+function test_nopluginsreq {
+    git checkout -q -b "$FUNCNAME" &&
+    test_exec_simple "rm -rf plugins-request.txt" "0" &&
+        test_expect_file_missing "plugins-request.txt" &&
+        test_expect_file_missing "plugins.txt" &&
+        true || return 1
+
+    # autoupdate without plugins-request.txt should fail
+    test_exec_simple ".github/workflows/autoupdate.sh" "1" &&
+        test_expect_file_missing "plugins-request.txt" &&
+        test_expect_file_missing "plugins.txt" &&
+
+    return 0
+}
+
+function test_nodockerfile {
+    git checkout -q -b "$FUNCNAME" &&
+    :
+}
+
 function test_bootstrap {
     # autoupdate with just bootstrap helm chart should work
-    export GITHUB_REF="refs/heads/$(git branch --show-current)"
-
-    test_exec_simple "mkdir -p charts/jenkins-lts-custom"
-    test_exec_simple "cp -a $BASEDIR/jenkins-lts-custom.bootstrap/* charts/jenkins-lts-custom"
+    git checkout -q -b "$FUNCNAME" &&
+    export GITHUB_REF="refs/heads/$(git branch --show-current)" &&
     test_exec_simple ".github/workflows/autoupdate.sh" &&
-        test_lastoutput_contains "::set-output name=newVersion::0.0.2-chartsubmodule-localtest.$(date "+%Y%m%d")"
-    export GITHUB_REF=
+        test_lastoutput_contains "::set-output name=newVersion::0.0.2-test-bootstrap.$(date "+%Y%m%d")" &&
+        test_expect_files "." "6" &&
+        test_expect_value "$(git status --porcelain)" "" &&
+    test_exec_simple "git log testmain..HEAD --oneline" &&
+        test_lastoutput_contains "Prepare release 0.0.2-test-bootstrap" &&
+        test_lastoutput_contains "Update Plugins" &&
+        test_lastoutput_contains "Bump to Jenkins Helm chart" &&
+
+    export GITHUB_REF= &&
+
+    true || return 1
 
     return 0
 }
